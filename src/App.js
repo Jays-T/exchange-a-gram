ae2f0dd7524620ced22522f740b039cdc4880485
John Traas <j.j.h.traas@gmail.com>
2022-02-15 22:11:47 +0100

    ADD react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Get your ideas out there On The Line!</h1>
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
