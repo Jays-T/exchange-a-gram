68841c617d42b00ee48822c99f5346cdec5a1517
John Traas <j.j.h.traas@gmail.com>
2022-02-15 20:48:29 +0100

    intial commit + react app setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Get your ideas out there On The Line!</h1>
+    </div>
+  );
+}
+
+export default App;
