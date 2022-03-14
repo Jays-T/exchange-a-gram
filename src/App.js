13b57ca133e8f751331e52e94d406de0594c2f76
John Traas <j.j.h.traas@gmail.com>
2022-03-14 15:43:26 +0100

    Setup Routing and basic NavBar + Add Axios Defaults

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import Button from 'react-bootstrap/Button';
-import './App.css';
+import React from 'react';
+import styles from './App.module.css';
+import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
+import './api/axiosDefaults'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Get your ideas out there On The Line!</h1>
-      <Button variant="primary">Primary</Button>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Get your ideas out there On The Line!</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+          <Route render={() => <h1>Page Not Found!</h1>} />
+        </Switch>
+        
+      </Container>
     </div>
   );
 }
