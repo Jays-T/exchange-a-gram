4e0db32def010f6f22c48e06785a1f984e91ba46
John Traas <j.j.h.traas@gmail.com>
2022-05-04 14:49:09 +0000

    Add initial PostsPage component and Routes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/posts/PostPage';
+import PostsPage from './pages/posts/PostsPage';
+import { useCurrentUser } from './context/CurrentUserContext';
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
   
   return (
     
@@ -17,7 +21,34 @@ function App() {
           <Container className={styles.Main}>
 
             <Switch>
-              <Route exact path="/" render={() => <h1>Get your ideas out there On The Line!</h1>} />
+              <Route 
+                exact 
+                path="/" 
+                render={() => <>
+                  <h1>Get your ideas out there On The Line!</h1>
+                  <PostsPage message="No results found. Adjust the search keyword."/>
+              </>} 
+              />
+              <Route 
+                exact 
+                path="/feed" 
+                render={() => <>
+                  <h1>Get your ideas out there On The Line!</h1>
+                  <PostsPage message="No results found. Adjust the search keyword or follow a user."
+                  filter={`owner__followed__owner__profile=${profile_id}&`}
+                  />
+              </>} 
+              />
+              <Route 
+                exact 
+                path="/liked" 
+                render={() => <>
+                  <h1>Get your ideas out there On The Line!</h1>
+                  <PostsPage message="No results found. Adjust the search keyword or like a post."
+                  filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+                  />
+              </>} 
+              />
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/posts/create" render={() => <PostCreateForm /> } />
